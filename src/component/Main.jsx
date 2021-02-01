9892411df370865638797d4e1a8ede2674d32bf4
Abdullahmad <kress1219@gmail.com>
2021-02-01 14:30:54 +0100

    code update

--- src/component/Main.jsx
+++ src/component/Main.jsx
@@ -5,27 +5,35 @@ const Main = () => {
         <div className="rounded-2xl">
           <video src="https://pagesooo.sfo2.cdn.digitaloceanspaces.com/videos/chungiyoo-thumb.mp4" className="rounded-xl"></video>
         </div>
+        <p className="text-pink-500 text-2xl mt-8 text font-medium">Chungi Yoo</p>
       </div>
-      <div className="p-6 border-t border-r border-gray-200">
-        <div> box</div>
+      <div className="p-12 border-t border-r border-gray-200">
+        <div><video src="https://pagesooo.sfo2.cdn.digitaloceanspaces.com/videos/d-d-n-a-thumb.mp4" className="rounded-xl"></video></div>
+        <p className="text-pink-500 text-2xl mt-8 text font-medium">DDNA</p>
       </div>
-      <div className="p-6 border-t border-r border-gray-200">
-        <div> box</div>
+      <div className="p-12 border-t border-r border-gray-200">
+        <div><video src="https://pagesooo.sfo2.cdn.digitaloceanspaces.com/videos/hiskin-thumb.mp4" className="rounded-xl"></video></div>
+        <p className="text-pink-500 text-2xl mt-8 text font-medium">BIZARRØ</p>
       </div>
-      <div className="p-6 border-t border-r border-gray-200">
-        <div> box</div>
+      <div className="p-12 border-t border-r border-gray-200">
+        <div><video src="https://pagesooo.sfo2.cdn.digitaloceanspaces.com/videos/bizar-thumb.mp4" className="rounded-xl"></video></div>
+        <p className="text-pink-500 text-2xl mt-8 text font-medium">Hi, skin</p>
       </div>
-      <div className="p-6 border-t border-r border-gray-200">
-        <div> box</div>
+      <div className="p-12 border-t border-r border-gray-200">
+        <div><video src="https://pagesooo.sfo2.cdn.digitaloceanspaces.com/videos/hiskin-thumb.mp4" className="rounded-xl"></video></div>
+        <p className="text-pink-500 text-2xl mt-8 text font-medium">Typography Principles</p>
       </div>
-      <div className="p-6 border-t border-r border-gray-200">
-        <div> box</div>
+      <div className="p-12 border-t border-r border-gray-200">
+        <div><img src="https://pagesooo.sfo2.cdn.digitaloceanspaces.com/images/browser-history-2020-thumb.jpg" alt="" className="rounded-xl"/></div>
+        <p className="text-pink-500 text-2xl mt-8 text font-medium">Browser History 2020</p>
       </div>
-      <div className="p-6 border-t border-r border-gray-200">
-        <div> box</div>
+      <div className="p-12 border-t border-r border-gray-200">
+        <div><img src="https://pagesooo.sfo2.cdn.digitaloceanspaces.com/images/howtotalktowhitekidsaboutracism-thumb.jpg" alt="" className="rounded-xl"/></div>
+        <p className="text-pink-500 text-2xl mt-8 text font-medium">How to Talk to White Kids about Racism</p>
       </div>
-      <div className="p-6 border-t border-r border-gray-200">
-        <div> box</div>
+      <div className="p-12 border-t border-r border-gray-200">
+        <div><img src="https://pagesooo.sfo2.cdn.digitaloceanspaces.com/images/takeboost-thumb.jpg" alt="" className="rounded-xl"/></div>
+        <p className="text-pink-500 text-2xl mt-8 text font-medium">BOOST</p>
       </div>
     </div>
   );
